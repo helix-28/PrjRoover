c835da1285e4d0db1562eb6ee51b2be027c01843
Félix <helixp28@gmail.com>
2025-03-12 15:30:41 -0400

    ajout timer 2 secondes et commande arret

--- RasberryPi-Commande/CommandeBase.py
+++ RasberryPi-Commande/CommandeBase.py
@@ -4,12 +4,9 @@ import time
 # https://www.electronicshub.org/raspberry-pi-l298n-interface-tutorial-control-dc-motor-l298n-raspberry-pi/
 
 
-
 import RPi.GPIO as GPIO
 
 from time import sleep
-
-
 
 moteurF_AVNT_DRT = 23
 
@@ -35,9 +32,9 @@ NSLEEP2 = 13
 
 GPIO.setmode(GPIO.BCM)
 
-GPIO.setup(NSLEEP1,GPIO.OUT)
+GPIO.setup(NSLEEP1, GPIO.OUT)
 
-GPIO.setup(NSLEEP2,GPIO.OUT)
+GPIO.setup(NSLEEP2, GPIO.OUT)
 
 GPIO.setup(moteurF_AVNT_DRT, GPIO.OUT)
 
@@ -57,9 +54,9 @@ GPIO.setup(moteurR_ARR_GCH, GPIO.OUT)
 
 GPIO.output(moteurF_AVNT_DRT, GPIO.LOW)  # roue avant droite avance
 
-GPIO.output(moteurR_AVNT_DRT, GPIO.LOW)	# roue avant gauche avance
+GPIO.output(moteurR_AVNT_DRT, GPIO.LOW)  # roue avant gauche avance
 
-GPIO.output(moteurF_AVNT_GCH, GPIO.LOW)	# roue avant droite recule
+GPIO.output(moteurF_AVNT_GCH, GPIO.LOW)  # roue avant droite recule
 
 GPIO.output(moteurR_AVNT_GCH, GPIO.LOW)  # roue avant gauche recule
 
@@ -71,9 +68,9 @@ GPIO.output(moteurF_ARR_GCH, GPIO.LOW)  # roue arriere gauche avance
 
 GPIO.output(moteurR_ARR_GCH, GPIO.LOW)  # roue arriere gauche recule
 
-p1=GPIO.PWM(NSLEEP1,1000)
+p1 = GPIO.PWM(NSLEEP1, 1000)
 
-p2=GPIO.PWM(NSLEEP2,1000)
+p2 = GPIO.PWM(NSLEEP2, 1000)
 
 p1.start(5)
 
@@ -87,14 +84,20 @@ print("r-run s-stop f-forward b-backward l-low m-medium h-high lt-lefttranslatio
 
 print("\n")
 
-
-
-while(1):
-
-
+while (1):
 
     x = input()
+    def arret():
 
+        # Arrêt du robot après 2 secondes
+        GPIO.output(moteurF_AVNT_DRT, GPIO.LOW)
+        GPIO.output(moteurR_AVNT_DRT, GPIO.LOW)
+        GPIO.output(moteurF_AVNT_GCH, GPIO.LOW)
+        GPIO.output(moteurR_AVNT_GCH, GPIO.LOW)
+        GPIO.output(moteurF_ARR_DRT, GPIO.LOW)
+        GPIO.output(moteurR_ARR_DRT, GPIO.LOW)
+        GPIO.output(moteurF_ARR_GCH, GPIO.LOW)
+        GPIO.output(moteurR_ARR_GCH, GPIO.LOW)
 
 
     if x == 'r':
@@ -120,7 +123,8 @@ while(1):
             GPIO.output(moteurR_ARR_GCH, GPIO.LOW)
 
             print("forward")
-            time.sleep(2)
+            sleep(2)
+            arret()
             x = 'z'
 
 
@@ -171,21 +175,7 @@ while(1):
 
         print("stop")
 
-        GPIO.output(moteurF_AVNT_DRT, GPIO.LOW)
-
-        GPIO.output(moteurR_AVNT_DRT, GPIO.LOW)
-
-        GPIO.output(moteurF_AVNT_GCH, GPIO.LOW)
-
-        GPIO.output(moteurR_AVNT_GCH, GPIO.LOW)
-
-        GPIO.output(moteurF_ARR_DRT, GPIO.LOW)
-
-        GPIO.output(moteurR_ARR_DRT, GPIO.LOW)
-
-        GPIO.output(moteurF_ARR_GCH, GPIO.LOW)
-
-        GPIO.output(moteurR_ARR_GCH, GPIO.LOW)
+        arret()
 
         x = 'z'
 
@@ -292,7 +282,7 @@ while(1):
 
         x = 'z'
 
-        
+
 
     elif x == 'rd':
 
@@ -318,7 +308,7 @@ while(1):
 
         x = 'z'
 
-        
+
 
     elif x == 'ld':
 
@@ -344,7 +334,7 @@ while(1):
 
         x = 'z'
 
-        
+
 
     elif x == 'ldb':
 
@@ -368,9 +358,9 @@ while(1):
 
         temp1 = 0
 
-        x = 'z'     
+        x = 'z'
 
-    
+
 
     elif x == 'rdb':
 
@@ -396,9 +386,9 @@ while(1):
 
         x = 'z'
 
-        
 
-    elif x=='l':
+
+    elif x == 'l':
 
         print("low")
 
@@ -406,11 +396,11 @@ while(1):
 
         p2.ChangeDutyCycle(25)
 
-        x='z'
+        x = 'z'
 
-        
 
-    elif x=='m':
+
+    elif x == 'm':
 
         print("medium")
 
@@ -418,11 +408,11 @@ while(1):
 
         p2.ChangeDutyCycle(50)
 
-        x='z'
+        x = 'z'
 
-        
 
-    elif x=='h':
+
+    elif x == 'h':
 
         print("high")
 
@@ -430,9 +420,8 @@ while(1):
 
         p2.ChangeDutyCycle(75)
 
-        x='z'
+        x = 'z'
 
-        
 
     elif x == 'e':
 
@@ -442,172 +431,8 @@ while(1):
 
         break
 
-
-
     else:
 
         print("<<<  wrong data  >>>")
 
         print("please enter the defined data to continue.....")
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
