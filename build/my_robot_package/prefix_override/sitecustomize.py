a5bcf137a458fefcdcd96acf2f675f4ff8c7d267
vikmo16 <vikmo16@gmail.com>
2025-04-09 15:58:36 -0400

    robot stack cmd manette

--- build/my_robot_package/prefix_override/sitecustomize.py
+++ build/my_robot_package/prefix_override/sitecustomize.py
@@ -1,4 +1,4 @@
 import sys
 if sys.prefix == '/usr':
     sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/vikmo/roover_ws/PrjRoover/install/my_robot_package'
+    sys.prefix = sys.exec_prefix = '/home/Roover/PrjRoover/install/my_robot_package'
