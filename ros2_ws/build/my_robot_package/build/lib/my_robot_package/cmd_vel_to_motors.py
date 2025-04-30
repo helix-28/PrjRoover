e746d684e3ee3b9e173f0f97b764d9d091e5f1e0
vikmo16 <vikmo16@gmail.com>
2025-04-30 14:27:43 -0400

    slam config

--- ros2_ws/build/my_robot_package/build/lib/my_robot_package/cmd_vel_to_motors.py
+++ ros2_ws/build/my_robot_package/build/lib/my_robot_package/cmd_vel_to_motors.py
@@ -51,14 +51,13 @@ class CmdVelToMotors(Node):
         self.p4.start(25)
 
     def listener_callback(self, msg):
-        vitesse = msg.linear.x * 100
-        angle = math.degrees(msg.angular.z)
-        self.get_logger().info(f"cmd_vel reçu : vitesse={vitesse:.2f}, angle={angle:.2f}")
-        self.direction_mecanum(vitesse, angle, 0)
+        vx = msg.linear.x * 100
+        vy = msg.linear.y * 100
+        rot = msg.angular.z * 100
+        self.get_logger().info(f"cmd_vel reçu : vx={vx:.2f}, vy={vy:.2f}, rot = {rot:.2f}")
+        self.direction_mecanum(vx, vy, rot)
 
-    def direction_mecanum(self, vitesse, angle, rotation):
-        vx = vitesse * math.cos(math.radians(angle))
-        vy = vitesse * math.sin(math.radians(angle))
+    def direction_mecanum(self,vx, vy,  rotation):
 
         v_AVNT_DRT = vx + vy + rotation
         v_AVNT_GCH = vx - vy - rotation
